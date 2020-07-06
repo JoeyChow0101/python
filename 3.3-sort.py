029cd0f740a138d53b21477623716c99830026e7
JoeyZhou <joeu.zhou2019@gmail.com>
2020-07-06 14:58:59 +0800

    Chapter 3

--- 3.3-sort.py
+++ 3.3-sort.py
@@ -1,3 +1,30 @@
-cars = ['bmw', 'benz', 'audi', ' toyota', 'gelly', 'byd']
+cars = ['bmw', 'benz', 'audi', 'toyota', 'gelly', 'byd']
 cars.sort()
 print(cars)
+cars.sort(reverse=True)
+print(cars)
+print(sorted(cars))
+cars.reverse()
+print(cars)
+#len = len(cars)
+#print(len)
+
+#3-8 放眼世界
+
+place = ['changbaishan', 'chifeng', 'kashi', 'lasa', 'qinghai', 'lijiang', 'sanya']
+print(place)
+print(sorted(place))
+print(place)
+print(sorted(place, reverse=True))
+print(place)
+place.reverse()
+print(place)
+place.reverse()
+print(place)
+place.sort()
+print(place)
+place.sort(reverse=True)
+print(place)
+
+#3-9 晚餐嘉宾
+print(len(place))
